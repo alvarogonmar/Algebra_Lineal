021ccdba1290ccba9184315248e84467d1efb58a
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-19 09:14:22 -0600

    Add examples of integer and float arrays in linear algebra module

--- linear_algebra1.py
+++ linear_algebra1.py
@@ -5,6 +5,10 @@ import numpy as np
 
 # Create different datatypes arrays
 A = np.array([[1, 2],
-             [3, 4]])
+             [3, 4]]) # int array
 
+print(A, "\n")
+
+A = np.array([[1.1, 2],
+             [3.4, 4]]) # float array
 print(A, "\n")
