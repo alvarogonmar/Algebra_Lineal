96fc1bfa21b26f927a7940b23b5fbc3a18721495
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-19 09:49:09 -0600

    Add matrix initialization and dimension printing to Gauss-Jordan example

--- gauss-jordan.py
+++ gauss-jordan.py
@@ -1,3 +1,14 @@
 import numpy as np
 # resolver un sistema de ecuaciones usando gauss-jordan
 print("Solve a system of equations")
+matrix = np.array([[2, 4, 6, 18],
+                  [4, 5, 6, 24],
+                  [3, 1, -2, 4]], dtype=float)
+print(matrix)
+
+# imprimir numero de
+
+dimMatrix = matrix.shape # dimensiones de la matriz
+numRows = dimMatrix[0] # numero de filas
+print(dimMatrix) # dimensiones de la matriz
+print(numRows) # numero de filas
