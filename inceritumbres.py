69fc1b9b84665199cd345340d53814c0c7dc9318
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-28 18:00:23 -0600

    Add sum_total_divided_by_n_minus_1 function to return total divided by n-1 in inceritumbres.py

--- inceritumbres.py
+++ inceritumbres.py
@@ -28,3 +28,5 @@ def x_minus_prom_squared(quantities):
     return (sum)
 
 def sum_total_divided_by_n_minus_1(quantities):
+    sum = sum_total(quantities)
+    return (sum / (len(quantities) - 1))
