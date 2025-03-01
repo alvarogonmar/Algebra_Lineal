e296502f16fc8932adad943f1bcf1f2401ad78d8
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-28 18:07:07 -0600

    Add resultado function placeholder in incertidumbres.py

--- inceritumbres.py
+++ inceritumbres.py
@@ -56,3 +56,5 @@ def incertidumbre_estandar(quantities, incertidumbre_b):
     incertidumbre_a = incertidumbre_A(quantities)
     incertidumbre_estandar = (incertidumbre_a ** 2 + incertidumbre_b ** 2) ** 0.5
     return(round(incertidumbre_estandar, 2))
+
+def resultado():
