55fb67846e8bab96c73b8a0092e1d5cf1e163bbf
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-08 09:35:53 -0600

    Add return statement for solution and iteration count in Gauss-Seidel method

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -14,3 +14,4 @@ def gauss_seidel(A, b, x0, tol, max_iterations):
         # Calculate the error index
         error = np.abs(1 - np.linalg.norm(x) / np.linalg.norm(x_new))
         if error < tol:
+            return x_new, k
