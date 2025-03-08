29ad405c91d70bd3ea00935169e5b5597c3d1ec2
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-08 09:34:38 -0600

    Add calculation for partial sum in Gauss-Seidel method iteration

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -7,3 +7,4 @@ def gauss_seidel(A, b, x0, tol, max_iterations):
     for k in range(max_iterations):
         x_new = np.copy(x)
         for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in range(i))
