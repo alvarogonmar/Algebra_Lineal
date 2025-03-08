f6a2d6d6742ca5feefbcf55da5ae2a56bdb8f920
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-08 09:34:48 -0600

    Add calculation for second sum in Gauss-Seidel method iteration

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -8,3 +8,4 @@ def gauss_seidel(A, b, x0, tol, max_iterations):
         x_new = np.copy(x)
         for i in range(n):
             s1 = sum(A[i][j] * x_new[j] for j in range(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
